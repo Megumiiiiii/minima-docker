5c4b78a1f845d0bd756697ae41e2a9af576a1e02
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-11-14 12:27:36 +0700

    Create minimad.sh

---
+++ minimad.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+clear
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+echo -e "\033[0;35m"
+echo " ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░                                                                     ";
+echo " ░░░░░████░░░░░░░░░░░░░░░████░░░░░                                                                     ";
+echo " ░░░░███░░░░░░░░░░░░░░░░░░░███░░░░                                                                     ";
+echo " ░░░███░░░░░░░░░░░░░░░░░░░░░███░░░                                                                     ";
+echo " ░░███░░░░░░░░░░░░░░░░░░░░░░░███░░                                                                     ";
+echo " ░███░░░░░░░░░░░░░░░░░░░░░░░░░███░                                                                     ";
+echo " ████░░░░░░░░░░░░░░░░░░░░░░░░░████                                                                     ";
+echo " ████░░░░░░░░░░░░░░░░░░░░░░░░░████                                                                     ";
+echo " ██████░░░░░░░███████░░░░░░░██████                                                                     ";
+echo " █████████████████████████████████                                                                     ";
+echo " █████████████████████████████████                                                                     ";
+echo " ░███████████████████████████████░                                                                     ";
+echo " ░░████░███████████████████░████░░                                                                     ";
+echo " ░░░░░░░███▀███████████▀███░░░░░░░ ██████╗░░█████╗░  ███╗░░██╗░█████╗░██████╗░███████╗                 ";
+echo " ░░░░░░████──▀███████▀──████░░░░░░ ██╔══██╗██╔══██╗  ████╗░██║██╔══██╗██╔══██╗██╔════╝                 ";
+echo " ░░░░░░█████───█████───█████░░░░░░ ██████╦╝██║░░╚═╝  ██╔██╗██║██║░░██║██║░░██║█████╗░░                 ";
+echo " ░░░░░░███████▄█████▄███████░░░░░░ ██╔══██╗██║░░██╗  ██║╚████║██║░░██║██║░░██║██╔══╝░░                 ";
+echo " ░░░░░░░███████████████████░░░░░░░ ██████╦╝╚█████╔╝  ██║░╚███║╚█████╔╝██████╔╝███████╗                 ";
+echo " ░░░░░░░░█████████████████░░░░░░░░ ╚═════╝░░╚════╝░  ╚═╝░░╚══╝░╚════╝░╚═════╝░╚══════╝                 ";
+echo " ░░░░░░░░░███████████████░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░█████████████░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░███████████░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░███──▀▀▀──███░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░███─█████─███░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░███─███─███░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░░█████████░░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░                                                                     ";
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo -e '\e[33mProject =\e[37m' Install Minima
+echo -e '\e[33mCommunity =\e[37m' BeritaCryptoo
+echo -e '\e[33mDiscord =\e[37m' https://discord.gg/beritacryptoonode
+echo -e '\e[33mTelegram Channel =\e[37m' https://t.me/BeritaCryptoo
+echo -e '\e[33mTelegram Group =\e[37m' https://t.me/BeritaaCryptooGroup
+echo -e '\e[33mTwitter =\e[37m' https://twitter.com/BeritaCryptoo
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Install docker... \e[0m" && sleep 1
+sudo apt update -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt install docker-ce
+
+echo -e "\e[1m\e[32m1. Add user... \e[0m" && sleep 1
+sudo adduser minima && sudo usermod -aG sudo minima && sudo usermod -aG docker minima && su - minima
+
+echo -e "\e[1m\e[32m1. Start... \e[0m" && sleep 1
+docker run -d -e minima_mdspassword=123 -e minima_server=true -v ~/minimadocker9001:/home/minima/data -p 9001-9004:9001-9004 --restart unless-stopped --name minima9001 minimaglobal/minima:latest
+
+echo -e "\e[1m\e[32m1. Install Auto update... \e[0m" && sleep 1
+docker run -d --restart unless-stopped --name watchtower -e WATCHTOWER_CLEANUP=true -e WATCHTOWER_TIMEOUT=60s -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+
+echo '╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬ </SUDAH> ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬'
